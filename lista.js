7c85f836263f4fd272ac900d6919938b12c9fe32
Gromorgood <169109081+GuidoOzuna@users.noreply.github.com>
2025-02-21 16:25:57 -0300

    Update and rename script.js to lista.js

---
+++ lista.js
@@ -0,0 +1,15 @@
+// lista.js
+const personas = [
+  {
+    id: 1,
+    nombre: 'Juan',
+    apellido: 'Pérez',
+    edad: 30,
+    fechaNacimiento: '1995-05-15',
+    lugarNacimiento: 'Asunción',
+    lugarFallecimiento: 'N/A'
+  },
+  // Añade más personas aquí
+];
+
+module.exports = personas;
