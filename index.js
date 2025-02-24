8f9fd899d93c2cf7c99838ff69385f4239c69a72
Gromorgood <169109081+GuidoOzuna@users.noreply.github.com>
2025-02-24 11:06:49 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // index.js
 const express = require('express');
+const cors = require('cors');  // Add this line
 const app = express();
 const port = 3000;
+
+app.use(cors());  // Add this line
 
 // Importa la lista de personas desde lista.js
 const personas = require('./lista');
