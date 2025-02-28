34b05df45088707487575fdb72d5d25ceea96dd4
Gromorgood <169109081+GuidoOzuna@users.noreply.github.com>
2025-02-28 14:22:13 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 const noticiasRouter = require('./noticias');
 
 // Configuración de middleware para servir archivos estáticos
 app.use(express.static('public'));
+
+// Middleware para manejar CORS
+app.use(cors());
 
 // Rutas
 app.use('/api/noticias', noticiasRouter);
